23781930b62a38820475c1688bb41c4e40cbd72e
Danila197 <levvyi197@gmail.com>
2023-05-06 14:13:24 +0500

    day3+HM

--- src/index.js
+++ src/index.js
@@ -4,8 +4,39 @@ import './index.html';
 import 'swiper/scss';
 import 'swiper/scss/pagination';
 import './index.scss';
+import { slidersInit } from './modules/sliders';
 
 // use modules
+
+slidersInit('.about__slider', {
+    pagination: {
+        el: '.about__slider-pagination',
+    }
+});
+
+slidersInit('.career__slider', {
+    pagination: {
+        el: '.career__slider-pagination',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 26
+        },
+        // when window width is >= 640px
+        1240: {
+            slidesPerView: 4,
+            spaceBetween: 20
+        }
+    }
+});
+
 
 const videoBG = document.querySelector('.video-bg');
 
